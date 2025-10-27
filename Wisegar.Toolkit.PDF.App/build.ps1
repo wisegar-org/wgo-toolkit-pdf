f1bfc83877023ec95459221bdf7f932eacb35a10
Yariel Rodriguez <yariel.re@gmail.com>
2025-10-27 10:37:11 +0100

    Update build script

--- Wisegar.Toolkit.PDF.App/build.ps1
+++ Wisegar.Toolkit.PDF.App/build.ps1
@@ -1,1 +1,3 @@
-dotnet publish -f net9.0-windows10.0.19041.0 -c Release -p:WindowsPackageType=None
+Remove-Item -Path "./publish" -Recurse
+# Trimming only works with self-contained deployments
+dotnet publish -f net9.0-windows10.0.19041.0 -c Release -p:WindowsPackageType=None --self-contained true -p:PublishSingleFile=false -p:PublishTrimmed=true -o ./publish
